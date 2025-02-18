c2625a204cfaf7a871015862bc9bfa29f9ee9494
jdemirci <jdemirci@iu.edu>
2025-02-18 16:25:47 -0500

    steps per trial more demonstrative

--- Random Walk Problems/2d_random_walk.py
+++ Random Walk Problems/2d_random_walk.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 while True:
     try:
-        trials = int(input("Enter the number of trials per sample: "))
-        if trials <= 0:
+        steps = int(input("Enter the number of steps per trial: "))
+        if steps <= 0:
             print("Please enter a positive number")
             continue
         break
@@ -13,6 +13,7 @@ while True:
 
 triallst = []
 sample = 100
+trials = 1000
 
 for s in range(sample):
     successes = 0
@@ -21,7 +22,7 @@ for s in range(sample):
         x = 0
         y = 0
         
-        for step in range(1000):
+        for step in range(steps):
             if rng.choice([0, 1]):
                 x += rng.choice([-1, 1])
             else:
@@ -38,7 +39,7 @@ plt.figure(figsize=(10, 6))
 binwidth = 0.05
 plt.hist(triallst, bins=range(int(min(triallst)), int(max(triallst)) + 1, 1),
          edgecolor="yellow", color="brown")
-plt.title(f'Distribution of Success Rates ({trials} trials per sample)')
+plt.title(f'Distribution of Success Rates ({steps} steps per trial sample)')
 plt.xlabel('Success Rate (%)')
 plt.ylabel('Frequency')
 plt.grid(True, alpha=0.3)
